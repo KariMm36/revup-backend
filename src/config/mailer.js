0553569e995e2dcdeb049d8a7e66dc1c43ac16f4
Karim Mohammed abdelraouf abdelmaged <karim222101402@nmu.edu.eg>
2026-05-05 23:24:35 +0300

    revert: revert mailer config to default (remove forced IPv4 resolution)

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,19 +1,9 @@
 'use strict';
 
-const dns = require('dns');
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false, // use STARTTLS
-  // Override DNS lookup to force IPv4 — Railway blocks outbound IPv6 (ENETUNREACH)
-  lookup: (hostname, options, callback) => {
-    dns.resolve4(hostname, (err, addresses) => {
-      if (err) return callback(err);
-      callback(null, addresses[0], 4);
-    });
-  },
+  service: 'gmail',
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
@@ -21,4 +11,3 @@ const transporter = nodemailer.createTransport({
 });
 
 module.exports = transporter;
-
